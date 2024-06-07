24ddb7340e4d4aedb8f4d6843badfa100e6c38ce
Pedro Teixeira <pedro.teixeira@minderacodeacademy.com>
2024-06-07 15:12:08 +0100

    Initial commit with new projects

---
+++ Rock-Scissors-Paper/src/com/mindera/mindswap/Main.java
@@ -0,0 +1,16 @@
+package com.mindera.mindswap;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    Player player1 = new Player("João");
+    Player player2 = new Player("Maria");
+
+    Game game = new Game(player1, player2);
+
+    game.start();
+
+    }
+}
