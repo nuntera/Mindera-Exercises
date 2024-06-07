6a9c64e6757e9dd08cf73b059767a14a389bdf12
Pedro Teixeira <pedro.teixeira@minderacodeacademy.com>
2024-06-07 15:14:33 +0100

    Delete Rock-Scissors-Paper directory

--- Rock-Scissors-Paper/src/com/mindera/mindswap/RandomGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.mindera.mindswap;
-
-public class RandomGenerator {
-
-    public static int generate(int min, int max){
-        return (int) (Math.random() * (max - min + 1) + min);
-    }
-}
