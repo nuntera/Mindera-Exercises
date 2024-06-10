63b0b734d240aec91bbe8fa0fab9311f977c1e09
Pedro Teixeira <pedro.teixeira@minderacodeacademy.com>
2024-06-10 16:02:25 +0100

    project reviewed - added Bonus requirement

--- Guessing-Number/src/com/mindera/mindswap/Game.java
+++ Guessing-Number/src/com/mindera/mindswap/Game.java
@@ -8,11 +8,15 @@ public class Game {
     private final Player player2;
 
     private int gameRound;
+    private int player1Wins;
+    private int player2Wins;
 
     public Game(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
         gameRound = 0;
+        player1Wins = 0;
+        player2Wins = 0;
     }
 
 
@@ -20,30 +24,46 @@ public class Game {
         System.out.println("Game is starting");
         printPlayersName(player1.getName(), player2.getName());
 
-        boolean winnerFound = false;
+        // Initial game number
+        int gameNumber = RandomGenerator.generate(MIN, MAX);
 
-        while (!winnerFound) {
+        while (player1Wins < 3 && player2Wins < 3) {
             gameRound++;
             System.out.println("Round " + gameRound);
 
-            int gameNumber = RandomGenerator.generate(MIN, MAX);
             int player1Number = this.player1.pickNumber(MIN, MAX);
             int player2Number = this.player2.pickNumber(MIN, MAX);
 
-            winnerFound = determineWinner(gameNumber, player1Number, player2Number);
+            boolean winnerFound = determineRoundWinner(gameNumber, player1Number, player2Number);
+            // Generate a new game number and reset guesses only if a winner is found
+            if (winnerFound) {
+                gameNumber = RandomGenerator.generate(MIN, MAX);
+                player1.resetGuesses();
+                player2.resetGuesses();
+            }
         }
+        printGameWinner();
     }
 
-    private boolean determineWinner(int gameNumber, int player1Number, int player2Number) {
+    private void printGameWinner() {
+        if (getPlayer1Wins() == 3) {
+            System.out.println(player1.getName() + " wins with " + getPlayer1Wins() + " victories.");
+        }
+        else System.out.println(player2.getName() + " wins with " + getPlayer2Wins() + " victories.");
+    }
+
+    private boolean determineRoundWinner(int gameNumber, int player1Number, int player2Number) {
         System.out.println("The game number is: " + gameNumber);
         System.out.println(this.player1.getName() + " number is: " + player1Number);
         System.out.println(this.player2.getName() + " number is: " + player2Number);
 
         if (gameNumber == player1Number) {
             System.out.println(player1.getName() + " wins this round!");
+            player1Wins++;
             return true;
         } else if (gameNumber == player2Number) {
             System.out.println(player2.getName() + " wins this round!");
+            player2Wins++;
             return true;
         } else {
             System.out.println("It's a tie! No winner this round.");
@@ -53,5 +73,14 @@ public class Game {
 
     private void printPlayersName(String player1Name, String player2Name) {
         System.out.println("Players: " + player1Name + " VS " + player2Name);
+    }
+
+
+    public int getPlayer1Wins() {
+        return player1Wins;
+    }
+
+    public int getPlayer2Wins() {
+        return player2Wins;
     }
 }
