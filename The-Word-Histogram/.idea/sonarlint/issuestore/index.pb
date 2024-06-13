479610e43a61ea90f88fdbbb7d2b30d1cf0a61a7
Pedro Teixeira <pedro.teixeira@minderacodeacademy.com>
2024-06-13 15:35:42 +0100

    first commit

---
+++ The-Word-Histogram/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+R
+"src/com/mindera/mindswap/Main.java,0/9/0927b335c45b479c059fe9165569347361539679
+[
++src/com/mindera/mindswap/WordHistogram.java,8/e/8eb835128d6fb778ec3d5130d95b5233c7bc2f47
---
+++ The-Word-Histogram/src/com/mindera/mindswap/WordHistogram.java
@@ -0,0 +1,36 @@
+package com.mindera.mindswap;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class WordHistogram implements Iterable<String> {
+    Map<String, Integer> wordHistogram = new HashMap();
+
+
+    public void analyseString(String toAnalyse) {
+        String[] onlyWords = toAnalyse.toLowerCase().split("\\s+");
+
+        for(int i = 0; i < onlyWords.length; ++i) {
+            String word = onlyWords[i];
+            if (wordHistogram.containsKey(word)) {
+                Integer count = wordHistogram.get(word);
+                wordHistogram.replace(word, count + 1);
+                continue;
+            }
+            this.wordHistogram.put(word, 1);
+        }
+        for (String word : wordHistogram.keySet())
+            System.out.println(wordHistogram.get(word));
+        }
+
+
+    public Integer get(String word) {
+        return wordHistogram.get(word);
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return wordHistogram.keySet().iterator();
+    }
+}
